29544afd3025be4ba3534d7a139c7b695c59e4e2
weykon <635883092@qq.com>
2023-05-29 15:58:18 +0800

    refactor: change post list back into SSR

---
+++ app/[user]/posts/loading.tsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+    return (
+        <div>
+            <p>Loading ...</p>
+        </div>
+    )
+}
+export default Loading;
--- app/[user]/posts/page.tsx
+++ app/[user]/posts/page.tsx
@@ -1,7 +1,7 @@
-import PostList from "@/components/post.list";
 import { getSupabase } from "@/server.supabse";
 import Link from "next/link";
 import { notFound } from "next/navigation";
+import { Suspense } from "react";
 
 type Props = {
     params: {
@@ -14,28 +14,26 @@ type Props = {
 
 // do not cache this page
 export const revalidate = 0;
-
 export default async function PostListPage(props: Props) {
+    const perPage = 4;
     const supabase = getSupabase()
-
-
     const { data: userdata } = await supabase.auth.getUser();
     if (!userdata.user) {
         notFound();
     }
 
     if (isNaN(Number(props.searchParams.p))) {
-        props.searchParams.p = '0'
+        props.searchParams.p = '1'
     }
-    console.log(Number(props.searchParams.p))
-    const { from, to } = getPagination(Number(props.searchParams.p), 4);
+    const current = Number(props.searchParams.p);
+    const { from, to } = getPagination(Number(props.searchParams.p) - 1, perPage);
     const { data: posts, count } = await supabase
         .from('posts')
         .select('id,title,created_at,owner', { count: "exact" })
         .eq('owner', userdata.user!.id)
         .order('created_at', { ascending: false })
         .range(from, to);
-    console.log(count);
+
     return (
         <div className="justify-center items-center text-center mt-8 flex flex-col">
             <p className=" text-lg">this is posts list page</p>
@@ -47,7 +45,41 @@ export default async function PostListPage(props: Props) {
                     new post
                 </Link>
             </div>
-            <PostList posts={posts ?? []} />
+            <ul className="w-full text-left">
+                {
+                    posts?.map((post: any) => (
+                        <a href={`posts/${post.id}`} key={post.id} >
+                            <li
+                                className="dark:hover:bg-slate-600 hover:bg-slate-300 my-5 border-zinc-300 rounded-lg border-2 mx-24 p-6"
+                            >
+                                <p className="font-semibold text-xl">
+                                    {post.title}
+                                </p>
+                                <p className="text-right mt-8">
+                                    {new Date(post.created_at).toLocaleString()}
+                                </p>
+                            </li>
+                        </a>
+                    ))
+                }
+            </ul>
+            <nav aria-label="Page navigation example">
+                <ul className="inline-flex -space-x-px">
+                    <li>
+                        <a href={`?p=${current == 1 ? 1 : current - 1}`} className="px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Previous</a>
+                    </li>
+                    {
+                        new Array(Math.ceil((count ?? 0) / perPage)).fill(0).map((_, i) => (
+                            <li key={i}>
+                                <a href={`?p=${i + 1}`} className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{i + 1}</a>
+                            </li>
+                        ))
+                    }
+                    <li>
+                        <a href={`?p=${count == current ? count : current + 1}`} className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">Next</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     )
 }
@@ -56,6 +88,6 @@ const getPagination = (page: number, size: number) => {
     const limit = size ? +size : 3
     const from = page ? page * limit : 0
     const to = page ? from + size - 1 : size - 1
-
     return { from, to }
 }
+
