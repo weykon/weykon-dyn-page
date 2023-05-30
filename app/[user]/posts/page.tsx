77d8e28a9ede0b40f5f449f92556974d02a93b32
weykon <635883092@qq.com>
2023-05-30 11:05:45 +0800

    new enter page

--- app/[user]/posts/page.tsx
+++ app/[user]/posts/page.tsx
@@ -25,6 +25,7 @@ export default async function PostListPage(props: Props) {
     if (isNaN(Number(props.searchParams.p))) {
         props.searchParams.p = '1'
     }
+
     const current = Number(props.searchParams.p);
     const { from, to } = getPagination(Number(props.searchParams.p) - 1, perPage);
     const { data: posts, count } = await supabase
@@ -71,7 +72,9 @@ export default async function PostListPage(props: Props) {
                     {
                         new Array(Math.ceil((count ?? 0) / perPage)).fill(0).map((_, i) => (
                             <li key={i}>
-                                <a href={`?p=${i + 1}`} className="px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">{i + 1}</a>
+                                <a href={`?p=${i + 1}`}
+                                    className={`${current == i + 1 ? 'bg-neutral-200' : 'bg-white'} px-3 py-2 leading-tight text-gray-500 border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+                                >{i + 1}</a>
                             </li>
                         ))
                     }
--- app/page.tsx
+++ app/page.tsx
@@ -12,27 +12,26 @@ export default async function ServerComponent() {
 
   return (
     <div className='pt-2 justify-center items-center w-full text-center flex flex-col'>
+      <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">Explore the vast and wonderful world!</h1>
+        <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-400">Welcome to my experimental blog! Register to view all posts, password-free and open source. All blog articles are only visible to logged-in users. Please refrain from writing personal data. Join me on my journey to improve my backend technology stack!</p>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+          <a href="weykon/posts" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+            Read My Posts
+            <svg aria-hidden="true" className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+          </a>
+          <div className="py-3 px-5 text-base font-medium text-start text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+            <p>if you want a quickly dialog, DM me.</p>
+            {/* <li>discord: </li> */}
+            <li>twitter: <u><a href="https://twitter.com/WeykonK">WeykonK</a></u></li>
+          </div>
+        </div>
+      </div>
       <div className="flex flex-col justify-center items-center text-left justify-items-center w-full py-2">
-        <div className="flex flex-col justify-start py-10 mx-5">
-          <p>Hi! this is my personal page.</p>
-          <p>it is built by supabase and next13</p>
-          <p>I am learing a lot things, this is my experiment for next13 in first time</p>
-          <p>if you any interest on my work, check my github profile. <u><a href="https://github.com/weykon">go!</a></u></p>
+        <div className="flex flex-col justify-start pt-10 pb-5 mx-5 text-sm">
+          <p>If you're interested in my work, please check out my GitHub profile.  <u><a href="https://github.com/weykon">go!</a></u></p>
           <p>if you want to contact me, send me a email. <u><a href="mailto:weykon@qq.com">weykon@qq.com</a></u></p>
         </div>
-
-        <ul className="dark:bg-slate-700 bg-slate-300 rounded-md w-80 border-2 border-neutral-400 p-2 text-slate-600">
-          <p>if you want a quickly dialog, DM me.</p>
-          <li>discord: </li>
-          <li>twitter: <u><a href="https://twitter.com/WeykonK">WeykonK</a></u></li>
-        </ul>
-      </div>
-      <div
-        className="border-2 border-black text-black dark:border-gray-300 rounded-md my-8 w-auto px-4 h-16 items-center justify-center flex flex-col"
-      >
-        <Link
-          href="weykon/posts"
-        > read my posts </Link>
       </div>
     </div>
   );
