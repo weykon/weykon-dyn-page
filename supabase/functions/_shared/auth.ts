0b775909e5ee3e8040e590ae6f64fc0ce2c70d05
weykon <635883092@qq.com>
2023-06-03 00:59:09 +0800

    change supabase-js by imports-map

--- supabase/functions/_shared/auth.ts
+++ supabase/functions/_shared/auth.ts
@@ -1,4 +1,4 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "@supabase/supabase-js";
 
 export async function checkAuth(req: Request) {
   try {
