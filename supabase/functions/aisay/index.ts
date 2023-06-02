c0e6bb3784342e88de7671b273a64012e98b9736
weykon <635883092@qq.com>
2023-06-03 01:02:23 +0800

    remove auth.ts in _shared

--- supabase/functions/aisay/index.ts
+++ supabase/functions/aisay/index.ts
@@ -2,7 +2,7 @@ import "xhr_polyfill";
 import { serve } from "std/server";
 import { CreateCompletionRequest } from "openai";
 import { corsHeaders } from "../_shared/cors.ts";
-import { checkAuth } from "../_shared/auth.ts";
+import { createClient } from "@supabase/supabase-js";
 
 serve(async (req) => {
   if (req.method === "OPTIONS") {
@@ -11,7 +11,7 @@ serve(async (req) => {
 
   const check = await checkAuth(req);
   console.log("check", check);
-  
+
   if (!check) {
     return new Response("Not allowed", { status: 403, headers: corsHeaders });
   }
@@ -36,3 +36,44 @@ serve(async (req) => {
     body: JSON.stringify(completionConfig),
   });
 });
+
+
+export async function checkAuth(req: Request) {
+  try {
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabaseClient = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get("SUPABASE_URL") ?? "",
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get("SUPABASE_ANON_KEY") ?? "",
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      {
+        global: {
+          headers: { Authorization: req.headers.get("Authorization")! },
+        },
+      },
+    );
+    // Now we can get the session or user object
+    const {
+      data: { user },
+    } = await supabaseClient.auth.getUser();
+
+    console.log('user, ', user)
+    if(!user){
+      return false
+    }
+
+    // And we can run queries in the context of our authenticated user
+    const { data, error } = await supabaseClient.from("ai_user_use").select("*")
+      .eq('id',user?.id)
+      .eq("can_use", true);
+      
+    if (error) throw error;
+    if (!data) throw new Error("User not allowed to use AI");
+    return true;
+  } catch (error) {
+    console.log(error)
+    return false;
+  }
+}
